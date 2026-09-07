0fb76a83419a5cc130da1f211b4957a0ccb346d9
Meyadur Rahman <100347980+MiyadurRahman@users.noreply.github.com>
2026-09-07 12:59:23 +0600

    Refactor fetch function and update HTML structure for better readability

--- fetch_function.js
+++ fetch_function.js
@@ -6,11 +6,11 @@
 
 //  so we use then
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
+/*fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then((response) => response.json()) // returns the Promise containing the parsed JSON data
     .then((data) => console.log(data)); // logs the actual object
 
-
+*/
 
 //fetch and send and console each data
 
@@ -19,6 +19,10 @@ function loadout(){
         .then((response) => response.json()) // returns the Promise containing the parsed JSON data
         .then((data) => console.log(data));
 }
+
+
+
+
 const btn = document.getElementById("btn");
 btn.addEventListener("click", loadout);
 
@@ -31,12 +35,39 @@ const loadpost=()=>{
 
 };
 
-
-const displayPost=(posts)=>{
+//display in console in inspect
+/*const displayPost=(posts)=>{
 
     // console.log(posts);
 
-    posts.forEach(posts=> {
-      console.log(posts)  ;
-    });
+    // posts.forEach(posts=> {
+    //   console.log(posts)  ;
+    // });
+//similar
+
+
+    for(let post of posts){
+        console.log(post);
+    }
+}*/
+
+const displayPost=(posts)=>{
+//    1.get the container
+    const postcontainer=document.getElementById("post-container");
+
+ 
+
+    for(let post of posts){
+        //  console.log(post.title);
+        // console.log(post.body);
+
+        //2.create html element
+        const li=document.createElement("li");
+        li.innerText=post.title;//puts the title in ul
+        console.log(li);
+        // 3. add li into container 
+        postcontainer.appendChild(li);
+
+    
+    }
 }
